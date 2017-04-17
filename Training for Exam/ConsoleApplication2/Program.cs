97eecfba5e4497945201d865b91a552beffb964e
viktorlazarov <viktorlazarov@abv.bg>
2017-04-17 20:06:52 +0300

    Revert "delete unnesecery items"
    This reverts commit 5e75103842be715b2f5bd20d9b085e97510f5857.

---
+++ Training for Exam/ConsoleApplication2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hoursNeeded = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int employees = int.Parse(Console.ReadLine());
+
+            double availableHours = Math.Floor(((days * 0.9) * 8) + (employees * (2 * days)));
+
+            if (availableHours >= hoursNeeded)
+            {
+                Console.WriteLine($"Yes!{availableHours-hoursNeeded} hours left.");
+
+            }
+            else
+            {
+                Console.WriteLine($"Not enough time!{hoursNeeded-availableHours} hours needed.");
+            }
+        }
+    }
+}
